d92bcd944cb2ff1db32ff8ca9ff02d1279ebdb0c
Pushman <pushman.pl@gmail.com>
2013-11-10 14:46:06 +0100

    Working generator Properties gen1 and min1

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,25 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
+  property("min1") = forAll { (a: Int) =>
     val h = insert(a, empty)
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val m = if (a < b) a else b
+    findMin(insert(a, insert(b, empty))) == m
+  }
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[A]
+    m <- oneOf(value(empty), genHeap)
+  } yield insert(k, m)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
