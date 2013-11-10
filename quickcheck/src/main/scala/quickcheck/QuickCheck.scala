126e73e81475cb52066248f9068e9146d9dd8029
Pushman <pushman.pl@gmail.com>
2013-11-10 19:25:25 +0100

    Working first assignment.

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,18 +19,71 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(a, insert(b, empty))) == m
   }
 
+  property("min3") = forAll { (a: Int, b: Int) =>
+    toList(insert(a, insert(b, empty))) == (if (a < b) List(a, b) else List(b, a))
+  }
+
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
-  property("remove1") = forAll { (a: Int) =>
+  property("delete1") = forAll { (a: Int) =>
     isEmpty(deleteMin(insert(a, empty)))
   }
 
+  property("delete2") = forAll { (h: H) =>
+    toList(h).tail == toList(deleteMin(h))
+  }
+
+  property("remove3") = forAll { (a: Int, b: Int) =>
+    findMin(deleteMin(insert(a, insert(b, empty)))) == (if (a < b) b else a)
+  }
+
+  property("sort1") = forAll { (h: H) =>
+    isSorted(h)
+  }
+
+  property("sort2") = forAll { (h: H) =>
+    isSorted(deleteMin(h))
+  }
+
   property("meld1") = forAll { (h1: H, h2: H) =>
-    val m = if (findMin(h1) < findMin(h2)) findMin(h1) else findMin(h2)
+    val m = min(findMin(h1), findMin(h2))
     findMin(meld(h1, h2)) == m
+  }
+
+  property("meld2") = forAll { (a: Int, b: Int) =>
+    val h = meld(insert(a, empty), insert(b, empty))
+    !isEmpty(deleteMin(h)) && isEmpty(deleteMin(deleteMin(h)))
+  }
+
+  property("meld3") = forAll { (h1: H, h2: H) =>
+    isSorted(meld(h1, h2))
+  }
+
+  property("meld4") = forAll { (h1: H, h2: H) =>
+    toList(meld(h1, h2)) == toList(meld(h2, h1))
+  }
+
+  def isSorted(h: H): Boolean = {
+    isSorted(toList(h))
+  }
+
+  def isSorted(h: List[A]): Boolean = {
+    if (h.isEmpty || h.size == 1)
+      true
+    else
+      h.head <= h.tail.head && isSorted(h.tail)
+  }
+
+  def min(a: Int, b: Int) = if (a < b) a else b
+
+  def toList(h: H): List[A] = {
+    if (isEmpty(h))
+      Nil
+    else
+      findMin(h) :: toList(deleteMin(h))
   }
 
   lazy val genHeap: Gen[H] = for {
